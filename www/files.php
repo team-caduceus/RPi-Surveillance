baf2b15543f0f596d9b2d1efa409bd7aef9472a8
team-caduceus <johnghav@outlook.com>
2014-05-21 17:07:10 +0200

    updated the website from php -> html

--- www/files.php
+++ www/files.php
@@ -1,15 +1,45 @@
-<?php include 'header.php'; ?>
-<?php include 'menu.php'; ?>
-	<?php
-	session_start();
-	if(isset($_SESSION['user']) && !empty($_SESSION['user'])){
-	?>
-	<?php 
-	}else{
-	 header("location:index.php");
-	}
-	?>
-
+	<html>
+	<head>
+		<title>RPi Cam Control</title>
+		<link rel="stylesheet" href="caduceus.css" />
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600' rel='stylesheet' type='text/css'>
+		<script type="text/javascript" src="script.js"></script>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+		<script src="classie.js"></script>
+	
+	<script>
+		function init() {
+			window.addEventListener('scroll', function(e){
+				var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+					shrinkOn = 25,
+					header = document.querySelector("header");
+				if (distanceY > shrinkOn) {
+					classie.add(header,"smaller");
+				} else {
+					if (classie.has(header,"smaller")) {
+						classie.remove(header,"smaller");
+					}
+				}
+			});
+		}
+		window.onload = init();
+	</script>
+	
+	</head>
+	<body onload="setTimeout('init();', 100);">
+	
+	<header>
+		<div class="container clearfix">
+			<h1 id="logo">
+				RPI Surveillance
+			</h1>
+			<nav>
+				<a href="index.html">Stream</a>
+				<a style="color: #ddd;" href="files.php">Files</a>
+			</nav>
+		</div>
+	</header><!-- /header -->
+	
   <div id="wrap">
   <article>
   <section>
@@ -48,5 +78,6 @@
 	</section>
   </article>
   </div>
-
-<?php include 'footer.php'; ?>
+  
+</body>
+</html>
