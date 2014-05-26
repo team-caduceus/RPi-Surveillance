79fc63275884d1fa7d1c3548423080398098c1b6
team-caduceus <johnghav@outlook.com>
2014-05-26 11:54:33 +0200

    updated website

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-bugfree-wookie
-==============
+Web based interface for controlling the Raspberry Pi Camera, includes motion detection, time lapse, and image and video recording.
 
-Caduceus RPI project
+All information on this project can be found here: http://www.raspberrypi.org/phpBB3/viewtopic.php?f=43&t=63276
+
+The wiki page can be found here:
+
+http://elinux.org/RPi-Cam-Web-Interface
--- www/preview.php
+++ www/preview.php
@@ -1,51 +1,11 @@
-	<html>
-	<head>
-		<title>RPi Cam Control</title>
-		<link rel="stylesheet" href="caduceus.css" />
-		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600' rel='stylesheet' type='text/css'>
-		<script type="text/javascript" src="script.js"></script>
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
-		<script src="classie.js"></script>
-	
-	<script>
-		function init() {
-			window.addEventListener('scroll', function(e){
-				var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-					shrinkOn = 25,
-					header = document.querySelector("header");
-				if (distanceY > shrinkOn) {
-					classie.add(header,"smaller");
-				} else {
-					if (classie.has(header,"smaller")) {
-						classie.remove(header,"smaller");
-					}
-				}
-			});
-		}
-		window.onload = init();
-	</script>
-	
-	</head>
- 
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>RPi Cam Download</title>
+  </head>
   <body>
-
-  	<header>
-		<div class="container clearfix">
-			<h1 id="logo">
-				RPI Surveillance
-			</h1>
-			<nav>
-				<a href="index.html">Stream</a>
-				<a href="preview.php">Files</a>
-			</nav>
-		</div>
-	</header>
- 
-	<div id="wrap">
-	<article>
-
-
-	<?php
+    <p><a href="index.html">Back</a></p>
+    <?php
       if(isset($_GET["delete"])) {
         unlink("media/" . $_GET["delete"]);
       }
@@ -54,16 +14,14 @@
         foreach($files as $file) unlink("media/$file");
       }
       else if(isset($_GET["file"])) {
-        echo "<div class='heading'><h2>Preview</h2></div>";
+        echo "<h1>Preview</h1>";
         if(substr($_GET["file"], -3) == "jpg") echo "<img src='media/" . $_GET["file"] . "' width='640'>";
-        else echo "<video width='732' controls><source src='media/" . $_GET["file"] . "' type='video/mp4'>Your browser does not support the video tag.</video>";
+        else echo "<video width='640' controls><source src='media/" . $_GET["file"] . "' type='video/mp4'>Your browser does not support the video tag.</video>";
         echo "<p><input type='button' value='Download' onclick='window.open(\"download.php?file=" . $_GET["file"] . "\", \"_blank\");'> ";
         echo "<input type='button' value='Delete' onclick='window.location=\"preview.php?delete=" . $_GET["file"] . "\";'></p>";
       }
     ?>
-    <div class="heading">
-	<h2>Files</h2>
-	</div>
+    <h1>Files</h1>
     <?php
       $files = scandir("media");
       if(count($files) == 2) echo "<p>No videos/images saved</p>";
@@ -77,8 +35,5 @@
         echo "<p><input type='button' value='Delete all' onclick='if(confirm(\"Delete all?\")) {window.location=\"preview.php?delete_all\";}'></p>";
       }
     ?>
-	
-	</article>
-	</div>
   </body>
 </html>
